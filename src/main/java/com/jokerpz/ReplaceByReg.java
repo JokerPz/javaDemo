38de815847c2d3f61cdb43c649028f5617d740e7
Jokerpz <lpinzhou@163.com>
2020-08-22 21:36:11 +0800

    replaceByReg

--- src/main/java/com/jokerpz/ReplaceByReg.java
+++ src/main/java/com/jokerpz/ReplaceByReg.java
@@ -1,4 +1,54 @@
 package com.jokerpz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ReplaceByReg {
+    //public static void main(String[] args) {
+    //    String reg = "<expire>.*?</expire>";
+    //    String str = "<a>222</a><expire>111111</expire><b>222</b>";
+    //    str = str.replaceAll(reg, "555");
+    //    System.out.println(str);
+    //}
+    /**
+     * 正则表达式匹配两个指定字符串中间的内容
+     * @param soap
+     * @return
+     */
+    public static List<String> getSubUtil(String soap, String rgex){
+        List<String> list = new ArrayList<String>();
+        Pattern pattern = Pattern.compile(rgex);// 匹配的模式
+        Matcher m = pattern.matcher(soap);
+        while (m.find()) {
+            list.add(m.group(1));
+            System.out.println(m.group(1));
+        }
+        return list;
+    }
+    /**
+     * 返回单个字符串，若匹配到多个的话就返回第一个，方法与getSubUtil一样
+     * @param soap
+     * @param rgex
+     * @return
+     */
+    public static String getSubUtilSimple(String soap,String rgex){
+        Pattern pattern = Pattern.compile(rgex);// 匹配的模式
+        Matcher m = pattern.matcher(soap);
+        while(m.find()){
+            return m.group(1);
+        }
+        return "";
+    }
+    /**
+     * 测试
+     * @param args
+     */
+    public static void main(String[] args) {
+        String str = "abc3443abcfgjhgabcgfjabc";
+        String rgex = "abc(.*?)abc";
+        //System.out.println(getSubUtil(str,rgex));
+        System.out.println(getSubUtilSimple(str, rgex));
+    }
 }
