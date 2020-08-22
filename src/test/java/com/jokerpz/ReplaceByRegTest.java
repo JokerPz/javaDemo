f9bac0fa233219651505f0af1733734b3810afa5
joker-mac <lpinzhou@163.com>
2020-08-22 22:08:24 +0800

    create junit test

---
+++ src/test/java/com/jokerpz/ReplaceByRegTest.java
@@ -0,0 +1,21 @@
+package com.jokerpz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReplaceByRegTest {
+
+    @Test
+    public void getSubUtil() {
+
+    }
+
+    @Test
+    public void getSubUtilSimple() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
